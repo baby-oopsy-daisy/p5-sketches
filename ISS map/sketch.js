327e9630ca85faa81c3ab99a2d15d6538caf9ab2
baby-oopsy-daisy <prem4300795@gmail.com>
2020-03-28 20:55:49 +0530

    addded api for map data

--- ISS map/sketch.js
+++ ISS map/sketch.js
@@ -1,4 +1,4 @@
-let url = "https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.json";
+let url = "http://api.open-notify.org/iss-now.json";
 let longitude, latitude;
 let min_latitude = -90, max_latitude = 90;
 let min_longitude = -180, max_longitude = 180;
@@ -6,22 +6,29 @@ let curr_position, prev_position;
 let flag = false;
 let first  =true;
 let data;
+let bg;
+let count = 10000;
 
+function preload(){
+	bg = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/0,0,0,0,0/600x400?access_token=pk.eyJ1IjoicHJlbWplZXQiLCJhIjoiY2s4YnF5aDcxMGZjbTNlb2Fha254ODg4ciJ9.1ueZ7mTfahwJTJr8V0QGaw");
+}
 
 function setup() {
 	// createCanvas(windowWidth, windowHeight);
 	// noCanvas();
-	createCanvas(400, 400);
+	
+	createCanvas(600, 400);
+	bg.resize(width, height);
 	curr_position = createVector(0,0);
 	prev_position = createVector(0,0);
-	background(0);
+	background(bg);
 }
 
 
-function draw() {
 	
 
-	setInterval(() => {
+	// setInterval(
+		const load = () => {
 			loadJSON(url, (data) => {
 			latitude = map(data.iss_position.latitude, min_latitude, max_latitude, height, 0);
 			longitude = map(data.iss_position.longitude, min_longitude, max_longitude, 0, width);
@@ -35,20 +42,42 @@ function draw() {
 				first = !first;
 			}
 			else{
+
 				prev_position.x = curr_position.x;
 				prev_position.y = curr_position.y;
 				curr_position.x = longitude;
 				curr_position.y = latitude;
+
+				if((prev_position.x - curr_position.y) > (width/2)){
+					prev_position.x = curr_position.x;
+					prev_position.y = curr_position.y;
+				}
+
 			}
 			// console.log(longitude, latitude);
 		});
 	
-	}, 10000);
+	}
+	// , 120000);
 
-	if(flag){
-		stroke(255);
-		line(prev_position.x, prev_position.y, curr_position.x, curr_position.y);
+	
+
+	
+function draw(){
+	// background(bg);
+	if (count >= 1000){
+	load();
+	count = 0;
 	}
 
-
+	if(flag){
+		stroke(255, 0, 0);
+		strokeWeight(3);
+		line(prev_position.x, prev_position.y, curr_position.x, curr_position.y);	
+		
+	}
+	count += 1;
 }
+
+
+
