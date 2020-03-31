f64a48329939d7ab3fcbacffa7a113d2e3d1aa5b
baby-oopsy-daisy <prem4300795@gmail.com>
2020-03-31 20:45:26 +0530

    to verify

--- ISSmap/sketch.js
+++ ISSmap/sketch.js
@@ -1,134 +1,145 @@
+//URL'S FOR THE APIS IN USE
 let url = "https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-now.json";
+let background_image = "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/0,0,0,0,0/600x400?access_token=pk.eyJ1IjoicHJlbWplZXQiLCJhIjoiY2s4YnF5aDcxMGZjbTNlb2Fha254ODg4ciJ9.1ueZ7mTfahwJTJr8V0QGaw";
+
+// VARIABLES TO STORER THE LATITUDE AND LONGITUDE OF THE ISS
 let longitude, latitude;
-let min_latitude = -90, max_latitude = 90;
-let min_longitude = -180, max_longitude = 180;
+let min_latitude = -90, max_latitude = 90; // RANGE OF LATITUDE
+let min_longitude = -180, max_longitude = 180; // RANGE OF LONGITUDE
 let curr_position, prev_position;
-let flag = false;
-let first  =true;
-let data;
-let bg;
-let count = 10000;
-let info;
-let par;
-let x, y;
+
+let flag = false;// TO INSURE THAT THE CODE INSIDE DRAW DOESN'T START BEFOR THE LOADING OF DATA THUS AVOIDING UNNECCESSARY ERRORS
+let first  =true;// FOR THE FIRST API CALL TO LOAD THE ISS POSITION
+let ready = false;
+
+let bg;// STORE THE BACKGROUND IMAGE
+
+//MANAGE THE HTML ELEMNTS
+let info;//FOR DISPLAYING THE CURRENT LONGITUDE AND LATITUDE 
+let par;//FOR MANAGING THE lOWER PARAGRAPH ELEMNT INSIDE THE YELLOW AREA
 
 
+// MASK IS FOR THE TRANSPARENT CANVAS ON TOP OF THE DEFAULT CANVAS
+//THE LINE OF TRACE IS DRAWN ON THE MASK
 let mask, cnv;
 
+// THE PRELOAD FUNCTION EXECUTES IN THE BEGINNING AND LOADS THE BACKGROUND IMG
 function preload(){
-	bg = loadImage("https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/0,0,0,0,0/600x400?access_token=pk.eyJ1IjoicHJlbWplZXQiLCJhIjoiY2s4YnF5aDcxMGZjbTNlb2Fha254ODg4ciJ9.1ueZ7mTfahwJTJr8V0QGaw");
+	bg = loadImage(background_image);
 
 }
 
+
 function setup() {
 
-	//ALLIGNS THE CANVAS TO THE CENTER
+	//CREATING AND ALLIGING THE CANVAS TO THE CENTER
 	cnv = createCanvas(600, 400);
 	mask  = createGraphics(600, 400);
-	
-	par = select("#div");
-	x = (windowWidth - width) / 2;
-	y = (windowHeight - 500) / 2;
+	let x = (windowWidth - width) / 2;
+	let y = (windowHeight - 500) / 2;
 	cnv.position(x, y);
 	mask.position(x,y);
 	mask.pixelDensity(1);
 	
 
-
+	// MANAGING THE DOM ELEMENTS
+	par = select("#div");
 	par.position(0,y+425);
 	par.size(windowWidth);
 	info = select("#info");
+
+	// INITIALIGING THE POSITIONS VECTOR
 	curr_position = createVector(0,0);
 	prev_position = createVector(0,0);
-	background(bg);
-	mask.clear();
+
+
+	background(bg);// THE DEFAULT BACKGROUND OF MAP
+	mask.clear();// MAKING THE BACKGROUND OF THE MASK CLEAR
 	
+	//LOAD THE ISS DATA ONCE AND SET AN INTERVAL TO CALL IT EVERY 30 SECOND
+	load();
+	setInterval(load, 30000);
 }
 
 
 	
 
+//LOADS THE ISS POSITION DATA FROM THE API
 	
 		const load = () => {
 			loadJSON(url, (data) => {
 
-			info.html("THE ISS is currently at : " + data.iss_position.latitude + "°N, " + data.iss_position.longitude + "°E");
-				
-			latitude = map(data.iss_position.latitude, min_latitude, max_latitude, height, 0);
-			longitude = map(data.iss_position.longitude, min_longitude, max_longitude, 0, width);
 			
-			if(first){
-				curr_position.x =longitude;
-				curr_position.y = latitude;
-				prev_position.x = longitude; 
-				prev_position.y = latitude;
+				
+			latitude = data.iss_position.latitude;
+			longitude = data.iss_position.longitude;
+			info.html("THE ISS is currently at : " + latitude + "°N, " + longitude + "°E");
+			let x = map(longitude, min_longitude, max_longitude, 0, width);
+			let y = map(latitude, min_latitude, max_latitude, height, 0);
+			
+			if(first){// FOR THE FIRST ITERATION
+				//SET THE CURRENT AND PREVIOUS POSITIONS AS SAME
+				curr_position.x =x;
+				curr_position.y = y;
+				prev_position.x = x; 
+				prev_position.y = y;
 				flag = true;
 				first = !first;
 			}
-			else{
+			else{// FOR THE ITERATIONS OTHER THAN FIRST
+				// SET THE PREV AS CURRENT AND THEN UPDATE THE CURRENT POSITION
 
 				prev_position.x = curr_position.x;
 				prev_position.y = curr_position.y;
-				curr_position.x = longitude;
-				curr_position.y = latitude;
+				curr_position.x = x;
+				curr_position.y = y;
 
+				// IF THE LINE CROSSES THE WIDTH
+				// THEN UPDATING LIKE THIS PREVENTS A LINE BEING DRAWN ACROSS THE CANVAS
+				//AS PREV WILL EQUAL TO WIDTH AND CURR WILL BE CLOSE TO 0 I.E BEGINING OF THE CANVAS
 				if((prev_position.x - curr_position.x) > (width/2)){
 					prev_position.x = curr_position.x;
 					prev_position.y = curr_position.y;
 				}
 
 			}
-			
+			ready = true;
 		});
 	
 	}
 	
 
 
-let currX = 10, currY=100, prevY = 100, prevX =0;
+
 	
+// THE DRAW FUNCTION EXECUTES FOREVER	
 function draw(){
 	
 	background(bg);
 	
-	if (count >= 5000){
-	load();
-	count = 0;
-	}
 
 	if(flag){
-
+		
+		// DRAW A SEMI-TRANSPARENT CIRCLE ATE THE CURRENT POSITION OF ISS
 		fill(255, 40);
-		stroke(255,50, 50,);
+		stroke(255,50, 50);
 		ellipse(curr_position.x, curr_position.y, 80, 80);
-
-		mask.stroke(255, 0, 0);
-		mask.strokeWeight(3);
-		mask.line(prev_position.x, prev_position.y, curr_position.x, curr_position.y);
-		image(mask, 0, 0);
 		
+
+		// THE READY VARIABLE INSURES THAT 
+		// THE LINE IS NOT BEING DRAWN FOR EACH ITERATION OF DRAW
+		// BUT ONLY ONCE WHEN THE POSITIONS ARE UPDATED
+
+		if(ready){
+			
+			mask.stroke(255, 0, 0);
+			mask.strokeWeight(3);
+			mask.line(prev_position.x, prev_position.y, curr_position.x, curr_position.y);
+			ready = !ready;
+		}
 		
-	}
-	count += 1.5;
-
-
-
-
-	//Extra for testing the create grapics
-	// 			fill(255, 40);
-	// 	stroke(255,50, 50,);
-	// 	ellipse(currX, currY, 45, 45);
-	
-	// 	mask.stroke(255, 0, 0);
-	// 	mask.strokeWeight(3);
-	// 	mask.line(prevX, prevY, currX, currY);
-	// 	image(mask, 0, 0);
-
-	// prevx =currX;
-	
-	// currX += 1;
-
-
+		image(mask, 0, 0);// DISPLAYS THE EXTRA CANVAS ON WHICH LINE IS DRAWN
+		}
 
 }
 
